joseph-reynolds/phosphor-user-manager

commit 87a6ab71de880d5e28b95a2077cb9d2758e037d9

Adapt to OpenBMC's Linux-PAM module changes
This is co-requisite with a commit which replaces pam_cracklib with
pam_pwquality and replaces pam_tally2 with pam_faillock.
See https://gerrit.openbmc-project.xyz/c/openbmc/openbmc/+/41357.

Adapt to the Linux-PAM module changes:
- The pam_pwquality.so module replaces the pam_cracklib.so module.
- The pam_faillock.so module replaces the pam_tally2.so module.
- The faillock command replaces the pam_tally2 command.

This adapts to the changed location of several configuration parameters
within the BMC's file system.  It moves them out of the /etc/pam.d/*
files and into config files for affected PAM modules.  Details are
below.

This changes the implementation of D-Bus "User" interfaces:
https://github.com/openbmc/phosphor-dbus-interfaces/blob/master/xyz/openbmc_project/User/
- /xyz/openbmc_project/User/Attributes.interface.yaml
    - UserLockedForFailedAttempt - uses the faillock command (was
      pam_tally2)
- /xyz/openbmc_project/User/AccountPolicy.interface.yaml
    - MaxLoginAttemptBeforeLockout - uses the pam_faillock module deny
      parameter (was pam_tally2)
    - AccountUnlockTimeout - used the pam_faillock module unlock_time
      parameter (was pam_tally2)
    - MinPasswordLength - uses the pam_pwquality module min parameter
      (was pam_cracklib)

This also fixes unrelated CI quality checks in bootstrap.sh.

The following shows properties whose implementation is affected by this
commit.  Each entry shows the correlation between:
 - Redfish resource type and property name.
 - D-Bus interface and property name.
 - The location of that property value within the BMC.

Redfish: ManagerAccount.UserLockedForFailedAttempt
D-Bus: xyz.openbmc_project.User.Attributes UserLockedForFailedAttempt
BMC Command:
  old: via the pam_tally2 command
  new: via the faillock command

Redfish: AccountService.MaxLoginAttemptBeforeLockout
D-Bus: xyz.openbmc_project.User.AccountPolicy MaxLoginAttemptBeforeLockout
File:

user_mgr.cpp
@@ -60,15 +60,17 @@ static constexpr int success = 0;
 static constexpr int failure = -1;
 
 // pam modules related
-static constexpr const char* pamTally2 = "pam_tally2.so";
-static constexpr const char* pamCrackLib = "pam_cracklib.so";
+static constexpr const char* pamFaillock = "pam_faillock.so";
+static constexpr const char* pamPWQuality = "pam_pwquality.so";
 static constexpr const char* pamPWHistory = "pam_pwhistory.so";
 static constexpr const char* minPasswdLenProp = "minlen";
 static constexpr const char* remOldPasswdCount = "remember";
 static constexpr const char* maxFailedAttempt = "deny";
 static constexpr const char* unlockTimeout = "unlock_time";
 static constexpr const char* pamPasswdConfigFile = "/etc/pam.d/common-password";
-static constexpr const char* pamAuthConfigFile = "/etc/pam.d/common-auth";
+static constexpr const char* faillockConfigFile = "/etc/security/faillock.conf";
+static constexpr const char* pwQualityConfigFile =
+    "/etc/security/pwquality.conf";
 
 // Object Manager related
 static constexpr const char* ldapMgrObjBasePath =
@@ -465,7 +467,7 @@ uint8_t UserMgr::minPasswordLength(uint8_t value)
     {
         return value;
     }
-    if (setPamModuleArgValue(pamCrackLib, minPasswdLenProp,
+    if (setPamModuleArgValue(pamPWQuality, minPasswdLenProp,
                              std::to_string(value)) != success)
     {
         log<level::ERR>("Unable to set minPasswordLength");
@@ -495,7 +497,7 @@ uint16_t UserMgr::maxLoginAttemptBeforeLockout(uint16_t value)
     {
         return value;
     }
-    if (setPamModuleArgValue(pamTally2, maxFailedAttempt,
+    if (setPamModuleArgValue(pamFaillock, maxFailedAttempt,
                              std::to_string(value)) != success)
     {
         log<level::ERR>("Unable to set maxLoginAttemptBeforeLockout");
@@ -510,8 +512,8 @@ uint32_t UserMgr::accountUnlockTimeout(uint32_t value)
     {
         return value;
     }
-    if (setPamModuleArgValue(pamTally2, unlockTimeout, std::to_string(value)) !=
-        success)
+    if (setPamModuleArgValue(pamFaillock, unlockTimeout,
+                             std::to_string(value)) != success)
     {
         log<level::ERR>("Unable to set accountUnlockTimeout");
         elog<InternalFailure>();
@@ -524,9 +526,16 @@ int UserMgr::getPamModuleArgValue(const std::string& moduleName,
                                   std::string& argValue)
 {
     std::string fileName;
-    if (moduleName == pamTally2)
+    bool simpleConfigFile = false;
+    if (moduleName == pamFaillock)
     {
-        fileName = pamAuthConfigFile;
+        fileName = faillockConfigFile;
+        simpleConfigFile = true;
+    }
+    else if (moduleName == pamPWQuality)
+    {
+        fileName = pwQualityConfigFile;
+        simpleConfigFile = true;
     }
     else
     {
@@ -555,7 +564,7 @@ int UserMgr::getPamModuleArgValue(const std::string& moduleName,
             // skip comments after meaningful section and process those
             line = line.substr(0, startPos);
         }
-        if (line.find(moduleName) != std::string::npos)
+        if (simpleConfigFile || (line.find(moduleName) != std::string::npos))
         {
             if ((startPos = line.find(argSearch)) != std::string::npos)
             {
@@ -577,9 +586,16 @@ int UserMgr::setPamModuleArgValue(const std::string& moduleName,
                                   const std::string& argValue)
 {
     std::string fileName;
-    if (moduleName == pamTally2)
+    bool simpleConfigFile = false;
+    if (moduleName == pamFaillock)
     {
-        fileName = pamAuthConfigFile;
+        fileName = faillockConfigFile;
+        simpleConfigFile = true;
+    }
+    else if (moduleName == pamPWQuality)
+    {
+        fileName = pwQualityConfigFile;
+        simpleConfigFile = true;
     }
     else
     {
@@ -612,7 +628,7 @@ int UserMgr::setPamModuleArgValue(const std::string& moduleName,
             // skip comments after meaningful section and process those
             line = line.substr(0, startPos);
         }
-        if (line.find(moduleName) != std::string::npos)
+        if (simpleConfigFile || (line.find(moduleName) != std::string::npos))
         {
             if ((startPos = line.find(argSearch)) != std::string::npos)
             {
@@ -664,52 +680,34 @@ void UserMgr::userEnable(const std::string& userName, bool enabled)
     return;
 }
 
-/**
- * pam_tally2 app will provide the user failure count and failure status
- * in second line of output with words position [0] - user name,
- * [1] - failure count, [2] - latest timestamp, [3] - failure timestamp
- * [4] - failure app
- **/
-
-static constexpr size_t t2UserIdx = 0;
-static constexpr size_t t2FailCntIdx = 1;
-static constexpr size_t t2OutputIndex = 1;
-
 bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
 {
     // All user management lock has to be based on /etc/shadow
     // TODO  phosphor-user-manager#10 phosphor::user::shadow::Lock lock{};
+
+    // Emulate the behavior of pam_faillock.so authsucc: get the number of
+    // failed attempts and compare with the deny= value
+    // See https://github.com/linux-pam/linux-pam/issues/327
     std::vector<std::string> output;
+    output = executeCmd("/usr/sbin/faillock", "--user", userName.c_str());
+    // Output is two header lines followed by zero or more records:
+    // > {userName}:
+    // > "When        Type    Source          Valid"
+    // > ${timestamp} ${type} {RHOST,TTY,SVC} {V,I}
+    //
+    // If there is an error, the message is like this:
+    // /usr/sbin/faillock: Error clearing the tally file for {user}:{output from
+    // perror}
+    int failedAttempts = output.size() - 2;
 
-    output = executeCmd("/usr/sbin/pam_tally2", "-u", userName.c_str());
-
-    std::vector<std::string> splitWords;
-    boost::algorithm::split(splitWords, output[t2OutputIndex],
-                            boost::algorithm::is_any_of("\t "),
-                            boost::token_compress_on);
-
-    try
+    if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
+        failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
     {
-        unsigned long tmp = std::stoul(splitWords[t2FailCntIdx], nullptr);
-        uint16_t value16 = 0;
-        if (tmp > std::numeric_limits<decltype(value16)>::max())
-        {
-            throw std::out_of_range("Out of range");
-        }
-        value16 = static_cast<decltype(value16)>(tmp);
-        if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
-            value16 >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
-        {
-            return true; // User account is locked out
-        }
-        return false; // User account is un-locked
+        log<level::ERR>("faillock resulted in error",
+                        entry("USER=%s", userName.c_str()));
+        elog<InternalFailure>();
     }
-    catch (const std::exception& e)
-    {
-        log<level::ERR>("Exception for userLockedForFailedAttempt",
-                        entry("WHAT=%s", e.what()));
-        throw;
-    }
+    return false; // User account password is un-locked
 }
 
 bool UserMgr::userLockedForFailedAttempt(const std::string& userName,
@@ -722,13 +720,16 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName,
     {
         return userLockedForFailedAttempt(userName);
     }
-    output = executeCmd("/usr/sbin/pam_tally2", "-u", userName.c_str(), "-r");
+    output =
+        executeCmd("/usr/sbin/faillock", "--user", userName.c_str(), "--reset");
 
-    std::vector<std::string> splitWords;
-    boost::algorithm::split(splitWords, output[t2OutputIndex],
-                            boost::algorithm::is_any_of("\t "),
-                            boost::token_compress_on);
-
+    if (!output.empty() && output[0].find("Error") != std::string::npos)
+    {
+        log<level::ERR>("faillock reset resulted in error",
+                        entry("USER=%s", userName.c_str()),
+                        entry("OUTPUT=%s", output[0].c_str()));
+        elog<InternalFailure>();
+    }
     return userLockedForFailedAttempt(userName);
 }
 
@@ -1174,7 +1175,8 @@ UserMgr::UserMgr(sdbusplus::bus::bus& bus, const char* path) :
     std::string valueStr;
     auto value = minPasswdLength;
     unsigned long tmp = 0;
-    if (getPamModuleArgValue(pamCrackLib, minPasswdLenProp, valueStr) !=
+
+    if (getPamModuleArgValue(pamPWQuality, minPasswdLenProp, valueStr) !=
         success)
     {
         AccountPolicyIface::minPasswordLength(minPasswdLength);
@@ -1225,7 +1227,8 @@ UserMgr::UserMgr(sdbusplus::bus::bus& bus, const char* path) :
         AccountPolicyIface::rememberOldPasswordTimes(value);
     }
     valueStr.clear();
-    if (getPamModuleArgValue(pamTally2, maxFailedAttempt, valueStr) != success)
+    if (getPamModuleArgValue(pamFaillock, maxFailedAttempt, valueStr) !=
+        success)
     {
         AccountPolicyIface::maxLoginAttemptBeforeLockout(0);
     }
@@ -1243,14 +1246,14 @@ UserMgr::UserMgr(sdbusplus::bus::bus& bus, const char* path) :
         }
         catch (const std::exception& e)
         {
-            log<level::ERR>("Exception for MaxLoginAttemptBeforLockout",
+            log<level::ERR>("Exception for MaxLoginAttemptBeforeLockout",
                             entry("WHAT=%s", e.what()));
             throw;
         }
         AccountPolicyIface::maxLoginAttemptBeforeLockout(value16);
     }
     valueStr.clear();
-    if (getPamModuleArgValue(pamTally2, unlockTimeout, valueStr) != success)
+    if (getPamModuleArgValue(pamFaillock, unlockTimeout, valueStr) != success)
     {
         AccountPolicyIface::accountUnlockTimeout(0);
     }

bootstrap.sh
@@ -16,4 +16,4 @@ esac
 
 autoreconf -i
 # shellcheck disable=SC2016
-echo 'Run "./configure ${CONFIGURE_FLAGS} && make"'
+echo "Run \"./configure \${CONFIGURE_FLAGS} && make\""

commit 54a1828bec0a9e9b77b0f8df8d81c3c1f8127d63

Fix UserLockedForFailedAttempt for locked users (#8)
* Fix faillock for locked users

This fixes a bug getting the xyz.openbmc_project.User.Attributes
property UserLockedForFailedAttempt when a password is locked.
And this only happens when the xyz.openbmc_project.User.AccountPolicy
MaxLoginAttemptBeforeLockout property has a nonzero value (default 5).

This bug was introduced in 87a6ab71de880d5e28b95a2077cb9d2758e037d9 titled
"Adapt to OpenBMC's Linux-PAM module changes".

This bug causes the phosphor Web app > Menu > Security and access > User
Management page to show no users, not allow users to be created, etc.

Also, when the faillock command fails with an error message, this
adds that error message to the journal.

Tested:
1. Locked a user (via the "login" command), and ensured that faillock
reported 5 failed attempts, and the D-Bus interface returned the
true value for the UserLockedForFailedAttempt property.
> busctl introspect xyz.openbmc_project.User.Manager \
>   /xyz/openbmc_project/user/admin

2. Removed the faillock file and observed the command failure.
> Change the faillock directory to a plain file:
> # mv /var/run/faillock /var/run/faillock-real
> # touch /var/run/faillock
> # faillock
> faillock: Error reading tally directory: Not a directory
> # busctl introspect xyz.openbmc_project.User.Manager
>   /xyz/openbmc_project/user/admin
> Failed to get all properties on interface
>   xyz.openbmc_project.User.Attributes: The operation failed internally.
> # journalctl (selected entries):
> phosphor-user-manager: /usr/sbin/faillock: Error opening the tally file
>   for admin:Not a directory
> phosphor-user-manager: The operation failed internally.
> Then to recover:
> # rm /var/run/faillock
> # mv /var/run/faillock-real /var/run/faillock

Signed-off-by: Joseph Reynolds <joseph-reynolds@charter.net>

* Updated log entry from faillock errors

user_mgr.cpp
@@ -695,17 +695,28 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
     // > "When        Type    Source          Valid"
     // > ${timestamp} ${type} {RHOST,TTY,SVC} {V,I}
     //
-    // If there is an error, the message is like this:
+    // If there is an error, the output is a single line like this:
     // /usr/sbin/faillock: Error clearing the tally file for {user}:{output from
     // perror}
-    int failedAttempts = output.size() - 2;
-
-    if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
-        failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
+    // Example: /usr/sbin/faillock: Error opening the tally file for admin:Not
+    // a directory
+    if (output.size() < 2)
     {
         log<level::ERR>("faillock resulted in error",
                         entry("USER=%s", userName.c_str()));
+        if (output.size() >= 1)
+        {
+            log<level::ERR>("faillock error message",
+                            entry("ERROR=%s", output[0].c_str()));
+        }
         elog<InternalFailure>();
+    }
+
+    int failedAttempts = output.size() - 2;
+    if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
+        failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
+    {
+        return true; // User account password is locked
     }
     return false; // User account password is un-locked
 }
54a1828bec0a9e9b77b0f8df8d81c3c1f8127d63
Joseph Reynolds <joseph-reynolds@charter.net>
2022-01-25 14:04:00 -0600

    Fix UserLockedForFailedAttempt for locked users (#8)
    * Fix faillock for locked users
    
    This fixes a bug getting the xyz.openbmc_project.User.Attributes
    property UserLockedForFailedAttempt when a password is locked.
    And this only happens when the xyz.openbmc_project.User.AccountPolicy
    MaxLoginAttemptBeforeLockout property has a nonzero value (default 5).
    
    This bug was introduced in 87a6ab71de880d5e28b95a2077cb9d2758e037d9 titled
    "Adapt to OpenBMC's Linux-PAM module changes".
    
    This bug causes the phosphor Web app > Menu > Security and access > User
    Management page to show no users, not allow users to be created, etc.
    
    Also, when the faillock command fails with an error message, this
    adds that error message to the journal.
    
    Tested:
    1. Locked a user (via the "login" command), and ensured that faillock
    reported 5 failed attempts, and the D-Bus interface returned the
    true value for the UserLockedForFailedAttempt property.
    > busctl introspect xyz.openbmc_project.User.Manager \
    >   /xyz/openbmc_project/user/admin
    
    2. Removed the faillock file and observed the command failure.
    > Change the faillock directory to a plain file:
    > # mv /var/run/faillock /var/run/faillock-real
    > # touch /var/run/faillock
    > # faillock
    > faillock: Error reading tally directory: Not a directory
    > # busctl introspect xyz.openbmc_project.User.Manager
    >   /xyz/openbmc_project/user/admin
    > Failed to get all properties on interface
    >   xyz.openbmc_project.User.Attributes: The operation failed internally.
    > # journalctl (selected entries):
    > phosphor-user-manager: /usr/sbin/faillock: Error opening the tally file
    >   for admin:Not a directory
    > phosphor-user-manager: The operation failed internally.
    > Then to recover:
    > # rm /var/run/faillock
    > # mv /var/run/faillock-real /var/run/faillock
    
    Signed-off-by: Joseph Reynolds <joseph-reynolds@charter.net>
    
    * Updated log entry from faillock errors
    
    This updates the log message sent when the faillock command fails to use the ERROR keyword.
    This also enhances the comments which explain the output from the faillock command when it fails with an error.

--- user_mgr.cpp
+++ user_mgr.cpp
@@ -695,17 +695,28 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
     // > "When        Type    Source          Valid"
     // > ${timestamp} ${type} {RHOST,TTY,SVC} {V,I}
     //
-    // If there is an error, the message is like this:
+    // If there is an error, the output is a single line like this:
     // /usr/sbin/faillock: Error clearing the tally file for {user}:{output from
     // perror}
-    int failedAttempts = output.size() - 2;
-
-    if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
-        failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
+    // Example: /usr/sbin/faillock: Error opening the tally file for admin:Not
+    // a directory
+    if (output.size() < 2)
     {
         log<level::ERR>("faillock resulted in error",
                         entry("USER=%s", userName.c_str()));
+        if (output.size() >= 1)
+        {
+            log<level::ERR>("faillock error message",
+                            entry("ERROR=%s", output[0].c_str()));
+        }
         elog<InternalFailure>();
+    }
+
+    int failedAttempts = output.size() - 2;
+    if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
+        failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
+    {
+        return true; // User account password is locked
     }
     return false; // User account password is un-locked
 }
